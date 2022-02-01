936aa5b3435b7c1cda3ec7289bcaecdfa1d61e48
vzaichkin <vzaichkin@intersog.com>
2022-02-01 10:31:33 +0200

    adds 2 and 3

---
+++ homework-2/main.js
@@ -0,0 +1,48 @@
+// 1. Ввести с клавиатуры 2 числа `a` и `b` (где `a <<< b`. Символ "<<<" - означает "сильно меньше" ). Запустить цикл перебора от `a` до `b`. Вывести в консоль квадраты чётных чисел.
+
+
+
+// 2. Заставить пользователя ввести с клавиатуры число (не строку и не NaN).
+// DONE!!!!!!!!!!
+// do {
+//     a = prompt('Enter number');
+// } while (isNaN(a));
+
+
+// 3. Проверить число на простоту. Число вводить с клавиатуры.
+// DONE!!!!!!!!!!!!
+// let num = prompt('Enter number');
+
+// let isSimple = true;
+// for (let i = 2; i < num; i++) {
+// 	if (num % i == 0) {
+// 		isSimple = false;
+// 		break;
+// 	}
+// }
+
+// if (isSimple) {
+//     console.log('Simple number: ', num);
+// }
+
+
+// 4. Посчитать сумму простых чисел от 0 до 250.
+let sum = 0;
+let min = 0;
+let max = 250;
+
+for (let i = min; i < max; i++) {
+  let isSimple = true;
+  
+  for (let j = min; j < i; j++) {
+    if (i % j == 0) {
+        isSimple = false;
+    }
+  }
+  
+  if (isSimple) {
+    sum += i;
+  }
+}
+
+console.log(sum);
