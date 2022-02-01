be1c0d3a11a6c2e575a83b1da092292aac9928e8
vzaichkin <vzaichkin@intersog.com>
2022-02-01 11:29:52 +0200

    hw 2 completed

--- homework-2/main.js
+++ homework-2/main.js
@@ -1,50 +1,56 @@
 // 1. Ввести с клавиатуры 2 числа `a` и `b` (где `a <<< b`. Символ "<<<" - означает "сильно меньше" ). Запустить цикл перебора от `a` до `b`. Вывести в консоль квадраты чётных чисел.
+a = parseInt(prompt('Enter a'));
 
+do {
+    b = parseInt(prompt('Enter b'));
+} while ((a + 10) > b);
 
+for (let i = a; i <= b; i++) {
+    if (i % 2 == 0) {
+        console.log(i * 2);
+      }
+}
 
 // 2. Заставить пользователя ввести с клавиатуры число (не строку и не NaN).
-// DONE!!!!!!!!!!
-// do {
-//     a = prompt('Enter number');
-// } while (isNaN(a));
+do {
+    a = prompt('Enter number');
+} while (isNaN(a));
 
 
 // 3. Проверить число на простоту. Число вводить с клавиатуры.
-// DONE!!!!!!!!!!!!
-// let num = prompt('Enter number');
+let num = prompt('Enter number');
 
-// let isSimple = true;
-// for (let i = 2; i < num; i++) {
-// 	if (num % i == 0) {
-// 		isSimple = false;
-// 		break;
-// 	}
-// }
+let isSimple = true;
+for (let i = 2; i < num; i++) {
+	if (num % i == 0) {
+		isSimple = false;
+		break;
+	}
+}
 
-// if (isSimple) {
-//     console.log('Simple number: ', num);
-// }
+if (isSimple) {
+    console.log('Simple number: ', num);
+}
 
 
 // 4. Посчитать сумму простых чисел от 0 до 250.
-// DONE!!!!!!!!!!!
-// let sum = 0;
-// let min = 0;
-// let max = 250;
+let sum = 0;
+let min = 0;
+let max = 250;
 
-// for (let i = min; i < max; i++) {
-//   let isSimple = true;
+for (let i = min; i < max; i++) {
+  let isSimple = true;
   
-//   for (let j = 2; j < i; j++) {
-//     if (i % j == 0) {
-//         isSimple = false;
-//         break;
-//     }
-//   }
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) {
+        isSimple = false;
+        break;
+    }
+  }
   
-//   if (isSimple) {
-//     sum += i;
-//   }
-// }
+  if (isSimple) {
+    sum += i;
+  }
+}
 
-// console.log(sum);
+console.log(sum);
